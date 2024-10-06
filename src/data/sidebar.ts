5da4c5cc4be3ce94c8df38b44d15538a153ee863
Yassafouad <yassayassa16@gmail.com>
2024-10-06 19:20:44 +0300

    first commit

---
+++ src/data/sidebar.ts
@@ -0,0 +1,46 @@
+export const subscriptions = [
+  {
+    channelName: "yassafouad",
+    id: "yassafouad",
+    imgUrl:
+      "https://yt3.googleusercontent.com/ytc/APkrFKb--NH6RwAGHYsD3KfxX-SAgWgIHrjR5E4Jb5SDSQ=s176-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    channelName: "yassafouad",
+    id: "yassafouad",
+    imgUrl:
+      "https://yt3.googleusercontent.com/ytc/APkrFKbpSojje_-tkBQecNtFuPdSCrg3ZT0FhaYjln9k0g=s176-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    channelName: "yassafouad",
+    id: "yassafouad",
+    imgUrl:
+      "https://yt3.ggpht.com/ytc/APkrFKZWeMCsx4Q9e_Hm6nhOOUQ3fv96QGUXiMr1-pPP=s48-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    channelName: "yassafouad",
+    id: "yassafouad",
+    imgUrl:
+      "https://yt3.ggpht.com/ytc/APkrFKa6XiLa13mMVPzkmmTBcgNPjjqCGPrY86KfJFmf5w=s48-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    channelName: "yassafouad",
+    id: "yassafouad",
+    imgUrl:
+      "https://yt3.ggpht.com/FjeN785fVWx0Pr6xCbwPhhq8hHj_gocc3FygDXYDEQgp2gE_FQzRNsFHFAjQ3oE-VJaeGR1a=s68-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    channelName: "yassafouad",
+    id: "yassafouad",
+    imgUrl:
+      "https://yt3.googleusercontent.com/ytc/APkrFKYcYswt_UhD7D0j6ddiQz6Gb8Q_vSJOjhYI0CoXSw=s176-c-k-c0x00ffffff-no-rj-mo",
+  },
+]
+
+export const playlists = [
+  { id: "1", name: "Frontend & Backend" },
+  { id: "2", name: "Favorites" },
+  { id: "3", name: "React" },
+  { id: "4", name: "Non-Dev" },
+  { id: "5", name: "TypeScript" },
+]
